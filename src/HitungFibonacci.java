c3bded11ebb8bd9b6220f8b81a357917f9ec6851
ASUS <ASUS@DESKTOP-9FMG8PT>
2018-09-12 12:58:20 -0700

    Tambah Tampil Hasil

--- src/HitungFibonacci.java
+++ src/HitungFibonacci.java
@@ -23,6 +23,8 @@ public class HitungFibonacci {
         int n = tampilInput();
         
         BigInteger hasil = fibo(n);
+        
+        tampilHasil(n, hasil);
     }
     
     private static int tampilInput() {
@@ -47,6 +49,10 @@ public class HitungFibonacci {
         }
         
         return hasil[n - 1];
+    }
+    
+    private static void tampilHasil(int n, BigInteger hasil) {
+        System.out.println("Bilangan Fibonacci ke-" + n + " : " + hasil);
     }
     
     
