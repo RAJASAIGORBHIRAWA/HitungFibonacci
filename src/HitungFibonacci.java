58ed621cb7ab62de473045e749d462ab9e363c60
ASUS <ASUS@DESKTOP-9FMG8PT>
2018-09-12 12:54:55 -0700

    Tambah Hitung Fibonacci

--- src/HitungFibonacci.java
+++ src/HitungFibonacci.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class HitungFibonacci {
@@ -13,13 +14,17 @@ public class HitungFibonacci {
         System.out.println("\nHitung Fibonacci");
         System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
     }
+    
     public static void main(String[] args) {
         String identitas = "Rajasa Igor Bhirawa / X RPL 3 / 33";
         
         tampilJudul(identitas);
         
         int n = tampilInput();
+        
+        BigInteger hasil = fibo(n);
     }
+    
     private static int tampilInput() {
         Scanner scanner = new Scanner(System.in);
         
@@ -27,6 +32,23 @@ public class HitungFibonacci {
         int n = scanner.nextInt();
         
         return n;
-        
     }
+    
+    private static BigInteger fibo(int n) {
+        
+        BigInteger[] hasil = new BigInteger[n];
+        
+        hasil[0] = BigInteger.ONE;
+        hasil[1] = BigInteger.ONE;
+        
+        for(int i = 2; i < n; i++)
+        {
+            hasil[i] = hasil[i - 1].add(hasil[i - 2]);
+        }
+        
+        return hasil[n - 1];
+    }
+    
+    
+    
 }
