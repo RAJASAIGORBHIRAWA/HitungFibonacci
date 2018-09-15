20971c5e838d7f07761a59969c2f54014cc6aef0
ASUS <ASUS@DESKTOP-9FMG8PT>
2018-09-15 06:57:32 -0700

    Deklarasi Program

---
+++ src/Tugas02.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+
+public class Tugas02 {
+    public static void main(String[] args) {
+        System.out.println(" 7 KG = Rp 10.000");
+        System.out.println(" 5 KG = Rp 7.000");
+        System.out.println(" 4 KG = Rp 5.000");
+        System.out.println(" 3 KG = Rp 3.000");
+        System.out.println(" 2 KG = Rp 2.000");
+        int[] berat = new int[]{10000, 7000, 5000, 3000, 2000};
+        
+        int berat = input();
+        
+        Proses(berat, Berat, Harga);
+    }
+    
+    
+    
+    
+}
