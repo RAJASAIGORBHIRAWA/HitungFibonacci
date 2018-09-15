3bd6673231b476ad90ad37367c96c757ea9fcc31
ASUS <ASUS@DESKTOP-9FMG8PT>
2018-09-15 06:19:28 -0700

    Deklarasi Program

---
+++ src/Tugas01.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Scanner;
+public class Tugas01 {
+    public static void main(String[] args) {
+        
+        System.out.println("Rajasa Igor Bhirawa / X RPL 3 / 33");
+        System.out.println("pecahannya adalah Rp 5.000, Rp 2.000, Rp 1.000, Rp 500, Rp 100");
+        int uang = input();
+        int[] Nominal = new int[]{5000, 2000, 1000, 500, 100};
+        
+        JumlahUang(uang, Nominal);
+    }
+    
+    
+
+    
+}
