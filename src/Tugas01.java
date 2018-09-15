e62793b29516acffd48e53c920d40dfcdcfb19dc
ASUS <ASUS@DESKTOP-9FMG8PT>
2018-09-15 06:28:28 -0700

    Scanner input

--- src/Tugas01.java
+++ src/Tugas01.java
@@ -16,6 +16,16 @@ public class Tugas01 {
         JumlahUang(uang, Nominal);
     }
     
+    private static int input()
+    {
+        Scanner baca = new Scanner(System.in);
+        
+        System.out.print("Jumlah Uang = Rp ");
+        int uang = baca.nextInt();
+        
+        return uang;
+    }
+    
     
 
     
